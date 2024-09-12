465d7b89997a6b6e20ff5b6c75f5c412b54d28c6
Andrei <dinamo1999@icloud.com>
2024-09-12 14:13:13 +0200

    changes

--- scripts/global.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const hamburger = document.getElementById("hamburger-menu");
-    const nav = document.getElementById("main-nav");
-
-    hamburger.addEventListener("click", function() {
-        nav.classList.toggle("active");
-    });
-});
